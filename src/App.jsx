2a083bf979fd9aa831a5b4a3eb7664ac3f4b984b
avverma9 <av95766@gmail.com>
2025-09-29 03:46:44 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
-
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/footer';
 import Header from './components/header';
-import Login from './components/login'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/login';
 import MyRide from './pages/myride/my_ride';
 
-function App() {
-
+function Layout() {
   return (
-    <> <Header/>
-      <Router>
-       
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path='/home' element={<MyRide/>}/>
-        </Routes>
-        <Footer/>
-      </Router>
+    <>
+      <Header />
+      <MyRide />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Login page without header/footer */}
+        <Route path="/" element={<Login />} />
+
+        {/* Home page with header/footer */}
+        <Route path="/home" element={<Layout />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
